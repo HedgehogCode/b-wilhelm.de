afe4eb40c448ca7f205a5443f534fa6e015cc673
Benjamin Wilhelm <benjamin.wilhelm@uni-konstanz.de>
2018-12-01 13:21:14 +0100

    Setup travis to deploy to gh-pages branch

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,3 +3,5 @@ set -e # halt script on error
 
 bundle exec rake jekyll:check
 bundle exec rake jekyll:check_external_links
+
+JEKYLL_ENV=production bundle exec jekyll build --destination site
--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-then
-  rsync -v --recursive --inplace --delete \
-	  -e "ssh -i .travis/id_rsa -o UserKnownHostsFile=.travis/known_hosts" \
-	  _site/ $USERNAME@$HOST:$1/
-fi
